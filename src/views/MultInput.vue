2545597e25c1c03d4408642541ba94186dc61630
abraaopinto <pxzabraao13@gmail.com>
2020-02-04 20:02:04 -0300

    add slots

--- src/views/MultInput.vue
+++ src/views/MultInput.vue
@@ -3,7 +3,31 @@
     <h1>Mult input</h1>
     <q-card>
       <q-card-section>
-        <q-input>
+        <q-input
+          bottom-slots
+          v-model="text"
+          label="Informe o CPF, CNPJ, Nome ou Razão Social!"
+          counter
+          maxlength="100"
+          :dense="dense"
+        >
+          <template v-slot:before>
+            <q-icon name="fas fa-user-circle" />
+          </template>
+
+          <template v-slot:append>
+            <q-icon
+              v-if="text !== ''"
+              name="close"
+              @click="text = ''"
+              class="cursor-pointer"
+            />
+            <q-icon name="search" />
+          </template>
+
+          <template v-slot:hint>
+            Field hint
+          </template>
 
         </q-input>
       </q-card-section>
