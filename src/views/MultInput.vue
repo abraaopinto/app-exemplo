b26e7adc89987407fab9d5c8a5c594f81b3ccb6e
abraaopinto <pxzabraao13@gmail.com>
2020-02-04 19:21:15 -0300

    criação do template do componente.

---
+++ src/views/MultInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page>
+    <h1>Mult input</h1>
+    <q-card>
+      <q-card-section>
+        <q-input>
+
+        </q-input>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+<script>
+export default {
+
+}
+</script>
