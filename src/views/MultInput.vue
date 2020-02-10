f5bccff1532e084a682639114aede3648d44f4a1
abraaopinto <pxzabraao13@gmail.com>
2020-02-10 20:34:08 -0300

    add rules

--- src/views/MultInput.vue
+++ src/views/MultInput.vue
@@ -4,12 +4,15 @@
     <q-card>
       <q-card-section>
         <q-input
+          type="text"
           bottom-slots
-          v-model="text"
+          v-model="value"
           label="Informe o CPF, CNPJ, Nome ou Razão Social!"
           counter
-          maxlength="100"
-          :dense="dense"
+          @keyup="validateInput()"
+          dense
+          minlength="3"
+          v-bind:maxlength="limiteMaximoCampo"
         >
           <template v-slot:before>
             <q-icon name="fas fa-user-circle" />
@@ -17,16 +20,25 @@
 
           <template v-slot:append>
             <q-icon
-              v-if="text !== ''"
+              v-if="value !== ''"
               name="close"
-              @click="text = ''"
+              @click="value = ''"
               class="cursor-pointer"
             />
-            <q-icon name="search" />
+            <q-icon
+              name="search"
+              @click="validateInput()"
+              class="cursor-pointer"
+            />
           </template>
 
-          <template v-slot:hint>
-            Field hint
+          <template
+            v-if=" value !== ''"
+            v-slot:hint
+          >
+            <p v-if=" cpf !== ''"> CPF: {{ cpf }}</p>
+            <p v-if=" cnpj !== ''"> CNPJ: {{ cnpj }}</p>
+            <p v-if=" nomeRazaoSocial !== ''"> Nome ou Razão Social: {{ nomeRazaoSocial }}</p>
           </template>
 
         </q-input>
@@ -36,6 +48,159 @@
 </template>
 <script>
 export default {
+  data () {
+    return {
+      value: '',
+      idFiscal: '',
+      cpf: '',
+      cnpj: '',
+      cnpjRaiz: '',
+      nomeRazaoSocial: '',
+      pesquisarPorRaizCnpj: false,
+      tipoPessoa: '',
+      limiteMaximoCampo: 100
+    }
+  },
+  methods: {
+    validateInput () {
+      const regexRegra1 = /[A-Zi]/i
 
+      // Regra 1 - somente nomeRazaoSocial recebe o valor de value pois o campo contem letras e numeros.
+      if (this.value.match(regexRegra1) !== null) {
+        this.limiteMaximoCampo = 100
+        this.nomeRazaoSocial = this.value
+        this.idFiscal = ''
+        this.cpf = ''
+        this.cnpj = ''
+        this.cnpjRaiz = ''
+      } else {
+        /*  Analise dos demais casos onde todos os digitos são numericos. */
+        // Reseta o valor de nomeRazaoSocial.
+        this.nomeRazaoSocial = ''
+        this.tipoPessoa = ''
+
+        // Limita o tamanho do campo para o maior formato.
+        this.limiteMaximoCampo = 18
+        // Realizar as validações para os demais variaveis.
+
+        // Verifica se é um IdFiscal
+        if (this.validarCPF(this.value) || this.validarCNPJ(this.value)) {
+          this.idFiscal = this.value
+        } else {
+          this.idFiscal = ''
+        }
+
+        // Verifica se é um CPF
+        if (this.validarCPF(this.value)) {
+          this.cpf = this.value
+          this.limiteMaximoCampo = 14
+        } else {
+          this.cpf = ''
+        }
+
+        // Verifica se é um CNPJ
+        if (this.validarCNPJ(this.value)) {
+          this.limiteMaximoCampo = 18
+          this.cnpj = this.value
+        } else {
+          this.cnpj = ''
+        }
+
+        // Verifica se é uma Raiz do CNPJ.
+        if (this.value.length <= 8 && this.pesquisarPorRaizCnpj) this.cnpjRaiz = this.value
+
+        // Verifica se o usuario não deseja consultar pela Raiz do CNPJ.
+        if (!this.pesquisarPorRaizCnpj) this.cnpjRaiz = ''
+
+        // Verifica se o usuario digitou mais de 8 caracteres.
+
+        if (this.value.length > 8 && this.pesquisarPorRaizCnpj) {
+          this.cnpjRaiz = ''
+          this.pesquisarPorRaizCnpj = false
+        }
+      }
+    },
+    validarCPF (pCpf) {
+      pCpf = pCpf.replace(/[^\d]+/g, '').padStart(11, '0')
+
+      if (pCpf.length !== 11 ||
+        pCpf === '00000000000' ||
+        pCpf === '11111111111' ||
+        pCpf === '22222222222' ||
+        pCpf === '33333333333' ||
+        pCpf === '44444444444' ||
+        pCpf === '55555555555' ||
+        pCpf === '66666666666' ||
+        pCpf === '77777777777' ||
+        pCpf === '88888888888' ||
+        pCpf === '99999999999') return false
+
+      // Valida 1o digito
+      var add = 0
+      var rev = 0
+
+      for (var i = 0; i < 9; i++) add += parseInt(pCpf.charAt(i)) * (10 - i)
+
+      rev = 11 - (add % 11)
+
+      if (rev === 10 || rev === 11) rev = 0
+
+      if (rev !== parseInt(pCpf.charAt(9))) return false
+
+      // Valida 2o digito
+      add = 0
+      for (var j = 0; j < 10; j++) add += parseInt(pCpf.charAt(j)) * (11 - j)
+
+      rev = 11 - (add % 11)
+
+      if (rev === 10 || rev === 11) rev = 0
+
+      if (rev !== parseInt(pCpf.charAt(10))) return false
+
+      return true
+    },
+    validarCNPJ (pCnpj) {
+      pCnpj = pCnpj.replace(/[^\d]+/g, '').padStart(14, '0')
+
+      // Elimina CNPJs invalidos conhecidos
+      if (pCnpj.length !== 14 ||
+        pCnpj === '00000000000000' ||
+        pCnpj === '11111111111111' ||
+        pCnpj === '22222222222222' ||
+        pCnpj === '33333333333333' ||
+        pCnpj === '44444444444444' ||
+        pCnpj === '55555555555555' ||
+        pCnpj === '66666666666666' ||
+        pCnpj === '77777777777777' ||
+        pCnpj === '88888888888888' ||
+        pCnpj === '99999999999999') return false
+
+      // Valida DVs
+      var tamanho = pCnpj.length - 2
+      var numeros = pCnpj.substring(0, tamanho)
+      var digitos = pCnpj.substring(tamanho)
+      var soma = 0
+      var pos = tamanho - 7
+      for (var i = tamanho; i >= 1; i--) {
+        soma += numeros.charAt(tamanho - i) * pos--
+        if (pos < 2) pos = 9
+      }
+      var resultado = soma % 11 < 2 ? 0 : 11 - soma % 11
+      if (resultado !== digitos.charAt(0)) return false
+      tamanho = tamanho + 1
+      numeros = pCnpj.substring(0, tamanho)
+      soma = 0
+      pos = tamanho - 7
+
+      for (var j = tamanho; j >= 1; j--) {
+        soma += numeros.charAt(tamanho - j) * pos--
+        if (pos < 2) pos = 9
+      }
+      resultado = soma % 11 < 2 ? 0 : 11 - soma % 11
+
+      if (resultado !== digitos.charAt(1)) return false
+      return true
+    }
+  }
 }
 </script>
