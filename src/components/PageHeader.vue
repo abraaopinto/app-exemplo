1759b7c2d8cc131972610e33b4c44b65a0da2795
Abraão Pinto <pxzabraao13@gmail.com>
2020-02-18 14:01:37 -0300

    criação do componente

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="page-header" class="q-pa-sm q-gutter-sm">
+    <q-breadcrumbs>
+      <q-breadcrumbs-el :label="currentRoute" />
+    </q-breadcrumbs>
+    <q-toolbar>
+      <q-toolbar-title>
+        {{ title }}
+      </q-toolbar-title>
+    </q-toolbar>
+  </div>
+</template>
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageHeader',
+  props: {
+    title: {
+      type: String,
+      required: false,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      currentRoute: window.location.pathname
+    }
+  }
+}
+</script>
