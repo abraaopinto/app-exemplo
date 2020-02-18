f8119064a364402349a558d35bc80f3d4ea9d028
abraaopinto <pxzabraao13@gmail.com>
2020-02-18 17:22:34 -0300

    add notify and dialog

--- src/quasar.js
+++ src/quasar.js
@@ -5,13 +5,24 @@ import lang from 'quasar/lang/pt-br.js'
 import '@quasar/extras/roboto-font/roboto-font.css'
 import '@quasar/extras/material-icons/material-icons.css'
 import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
-import { Quasar } from 'quasar'
+import {
+  Quasar,
+  Notify,
+  Dialog
+} from 'quasar'
 
 Vue.use(Quasar, {
-  config: {},
   components: { /* not needed if importStrategy is not 'manual' */ },
   directives: { /* not needed if importStrategy is not 'manual' */ },
   plugins: {
+    Notify,
+    Dialog
   },
-  lang: lang
+  lang: lang,
+  config: {
+    notify: {
+      position: 'top-right',
+      actions: [{ icon: 'close', color: 'white' }]
+    }
+  }
 })
